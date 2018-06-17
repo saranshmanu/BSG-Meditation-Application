98e1c9d48cfe3e39c8517d0f7ee8091509cbe580
Saransh Mittal <saranshmanu@yahoo.co.in>
2018-06-17 20:16:50 +0530

    Added Gesture Control using Finger Point

--- BSG/ViewController.swift
+++ BSG/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var bottomHeightConstraint: NSLayoutConstraint!
+    
+    @IBAction func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            let translation = gestureRecognizer.translation(in: self.view)
+//            gestureRecognizer.view!.center = CGPoint(x: gestureRecognizer.view!.center.x + translation.x, y: gestureRecognizer.view!.center.y + translation.y)
+            bottomHeightConstraint.constant = bottomHeightConstraint.constant - translation.y
+            gestureRecognizer.setTranslation(CGPoint.zero, in:t self.view)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        self.bottomView.addGestureRecognizer(gestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {

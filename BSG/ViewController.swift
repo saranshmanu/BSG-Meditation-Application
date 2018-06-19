584a38ab2bdb272c78acaf923679f470026d977a
Saransh Mittal <saranshmanu@yahoo.co.in>
2018-06-19 22:47:52 +0530

    Home page UI completed

--- BSG/ViewController.swift
+++ BSG/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var bottomHeightConstraint: NSLayoutConstraint!
     
+    @IBAction func beginTheSession(_ sender: Any) {
+    }
+    
     @IBAction func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
         if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
             let translation = gestureRecognizer.translation(in: self.view)
 //            gestureRecognizer.view!.center = CGPoint(x: gestureRecognizer.view!.center.x + translation.x, y: gestureRecognizer.view!.center.y + translation.y)
             bottomHeightConstraint.constant = bottomHeightConstraint.constant - translation.y
-            gestureRecognizer.setTranslation(CGPoint.zero, in:t self.view)
+            gestureRecognizer.setTranslation(CGPoint.zero, in: self.view)
         }
     }
 
